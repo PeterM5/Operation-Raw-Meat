600ebb1686dae69e3a60e6ed522da1c66ee1a67f
PeterM5 <peter.maltzoff@gmail.com>
2021-06-13 18:18:47 +0200

    Added GIANT ENEMY SPIDER

--- Assets/Scenes/Enemy.cs
+++ Assets/Scenes/Enemy.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public float min_chase_distance;
+    public float speed;
+    private GameObject player;
+    private Rigidbody rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player3d");
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Check distance between enemy and player
+        float distance = Vector3.Distance(transform.position, player.transform.position);
         
+        // If player is close enough, chase!
+        if (distance < min_chase_distance) {
+            transform.LookAt(player.transform.position);
+            transform.eulerAngles = new Vector3 (0, transform.eulerAngles.y, 0);
+            rigidbody.AddForce((player.transform.position - transform.position) * speed / distance, ForceMode.Force);
+        }
     }
 
     void OnTriggerEnter(Collider col)
